c45ca89db803d2eea406ee8ca0bc6bd661d35e7e
meshadmh <147321583+meshadmh@users.noreply.github.com>
2023-10-29 17:11:32 +0000

    Slot Machine.py
    A simple slot machine game (runs in terminal) that outputs bets and winnings

---
+++ Slot Machine.py
@@ -0,0 +1,161 @@
+import random #randomises slot
+
+MAX_LINES = 3 # maximum lines bet on
+MAX_BET = 100 # max amount you can bet
+MIN_BET = 1 # min amount to bet on
+
+ROWS = 3 # slot machine
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+symbol_value = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+#check winnings from bet
+def check_winnings(columns,lines,bet,values):
+  winnings = 0
+  winning_lines = []
+  for line in range(lines):
+    symbol = columns[0][line]
+    for column in columns:
+      symbol_to_check = column[line]
+      if symbol != symbol_to_check:
+        break
+    else:
+      winnings += values[symbol] * bet
+      winning_lines.append(line + 1)
+
+  return winnings,winning_lines
+
+#one spin on slot machine
+
+def get_slot_machine_spin(rows,cols,symbols):
+  all_symbols = []
+  #gives key and value in dict below
+  for symbol, symbol_count in symbols.items():
+    for _ in range(symbol_count):
+      all_symbols.append(symbol)
+
+#picks a random value for each row
+#loops through values randomly
+#removes picked value and adds to column list
+    columns = []
+    for col in range(cols):
+      column = []
+      current_symbols = all_symbols[:]
+      for row in range(rows):
+        if len(current_symbols) > 0:
+          value = random.choice(current_symbols)
+          current_symbols.remove(value)
+          column.append(value)
+
+      columns.append(column)
+
+  return columns
+
+
+#prints the slot machine - for each row its loops every column
+def print_slot_machine(columns):
+  for row in range(len(columns[0])):
+    #gives index and items (enumerate)
+    for i, column in enumerate(columns):
+      if i != len(columns) - 1:
+        print(column[row], end=" | ")
+      else:
+        print(column[row], end="")
+    print()
+
+# how much money is deposited in pounds
+def deposit():
+  while True:
+    amount = input("What would you like to deposit? £")
+    if amount.isdigit():
+      amount = int(amount)
+      if amount > 0:
+         break
+      else:
+        print("Amount must be greater than 0")
+    else:
+        print("Please enter a number.")
+
+  return amount
+
+#number of lines to bet on
+def get_number_of_lines():
+  while True:
+    lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+    if lines.isdigit():
+      lines = int(lines)
+      if 1 <= lines <= MAX_LINES:
+         break
+      else:
+        print("Enter a valid number of lines")
+    else:
+        print("Please enter a number.")
+
+  return lines
+
+
+# second function for lines bet on
+
+def get_bet():
+  while True:
+    amount = input("What would you like to bet on each line? £")
+    if amount.isdigit():
+       amount = int(amount)
+       if MIN_BET <= amount <= MAX_BET:
+         break
+       else:
+        print(f"Amount must be between £{MIN_BET} and £{MAX_BET}.")
+    else:
+        print("Please enter an amount.")
+
+  return amount
+
+# turn on the slot machine/spin
+
+def spin(balance):
+  lines = get_number_of_lines()
+  while True:
+    bet = get_bet()
+    total_bet = bet * lines
+
+    if total_bet > balance:
+      print(f"You do not have enough to bet that amount, your current balance is: £{balance}")
+    else:
+      break
+
+
+  print(
+      f"You are betting £{bet} on {lines} lines. Total bet is equal to £{total_bet}")
+
+  slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+  print_slot_machine(slots)
+  winnings,winning_lines = check_winnings(slots,lines,bet,symbol_value)
+  print(f"You won £{winnings}.")
+  print(f"You won on lines:", *winning_lines) #unpack operator/splat operator
+  return winnings - total_bet # won or lost per spin
+
+def main(): #start up of game - deposit amount and left over & start game or quit sequence
+  balance = deposit()
+  while True:
+    print(f"Current balance is: £{balance}")
+    answer = input("Press enter to play (q to quit).")
+    if answer == "q":
+      print("Goodbye!")
+      break
+    balance += spin(balance)
+
+    print(f"You left with £{balance}")
+
+
+main()
